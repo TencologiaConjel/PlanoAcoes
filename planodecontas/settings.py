22d05c4165a7889d79ca273b8679a0ace654281c
renan melo <renan.melo@conjel.com.br>
2025-08-22 09:25:24 -0300

    Ajustando o requirements.txt

--- planodecontas/settings.py
+++ planodecontas/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 from decouple import config
 import os
 
@@ -51,12 +52,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'planodecontas.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 AUTH_PASSWORD_VALIDATORS = [
